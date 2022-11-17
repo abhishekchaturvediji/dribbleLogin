72b10ca34b70fdd2b8c330ff1090376d9acbac3f
abhishekchaturvediji <abhichaube321@gmail.com>
2022-11-17 23:17:14 +0530

    17-11 validation completed

--- myapp/src/controller/userController.js
+++ myapp/src/controller/userController.js
@@ -1,16 +1,33 @@
-const { RavRegisteredUser , UserTweets } = require('../models/index')
+const Joi = require('joi');
+
+
+// validation
+// checks
+// third process
+// response
 
 module.exports = {
 
-    createTweet : async(req,res) =>{
-        UserTweets.findAll()
-        .then(data => {
-          // console.log(":: data :; ", data);
-          res.render('tweet', {AllTweet : data})
+    userRegister : async(req,res) =>{
+
+        const reqParam = req.body;
+
+        const schema = Joi.object({
+            name: Joi.string().min(1).max(30).required().label('Name'),
+            email: Joi.string().email().required().label('Email'),
+            password: Joi.string().required().label('Password'),
+            confirm_password: Joi.ref('password'),
+            mobile_no: Joi.number().optional().label('Mobile Number'),
         })
-        .catch(err =>{
-          console.log("err :: ", err);
-        })
-    }
+
+        const { error, value } = schema.validate(reqParam);
+
+        if(error){
+            console.log(" error :: ", error.details[0].message);
+        }
+
+        
+
+    },
 
 }
--- myapp/src/routes/index.js
+++ myapp/src/routes/index.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var router = express.Router();
 const axios = require('axios');
-const { RavRegisteredUser , UserTweets } = require('../models/index')
+// const { RavRegisteredUser , UserTweets } = require('../models/index')
 
-const {createTweet} = require("../controller/userController")
+const {userRegister} = require("../controller/userController")
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -17,79 +17,12 @@ router.get('/login', function(req, res, next) {
   res.render('login',{ title : 'Login' });
 });
 
-router.post('/login', function(req, res) {
-  res.redirect('/login')
+router.get('/register', function(req, res, next) {
+  res.render('register');
 });
 
-router.get('/homepage', function(req, res) {
-  res.render('home')
-});
 
-router.post('/homepage', function(req, res) {
-  res.render('home', {email : req.body.email , password : req.body.password})
-});
-
-router.get('/three', function(req, res) {
-  res.render('three')
-});
-
-router.get('/tweet',createTweet);
-
-// let tweets = []; 
-router.post('/add-tweet',function(req, res) {
-
-  console.log("req.body :: ", req.body);
-
-  UserTweets.create({
-    tweets : req.body.tweet
-  })
-  .then(data =>{
-    console.log(": data created :");
-    console.log(":: data ::", data);
-    res.redirect('tweet')
-  })
-  .catch(err =>{
-    console.log(": data err :");
-    console.log(":: err :: ", err);
-  })
-
-});
-
-router.get('/delete-tweet', function(req, res) {
-  
-  let primartId = req.query.id; 
-  UserTweets.destroy({
-    where: {
-     id : primartId
-    }
-  })
-    .then(data => {
-      res.redirect('/tweet')
-    })
-    .catch(err =>{
-      console.log("err :: ", err);
-    })
-
-});
-
-// register
-router.get('/register', function(req, res){
-  res.render('register',{Message : ''})
-});
-
-router.post('/register-user', function(req, res){
-  if(req.body.name === ''){
-    return res.render('register',{Message : 'The name must not be empty'})
-  }
-  if(req.body.mobile_no === ''){
-    return res.render('register',{Message : 'The Mobile no must not be empty'})
-  }
-  if(typeof req.body.mobile_no != 'number' ){
-    return res.render('register',{Message : 'The Mobile no must a number'})
-  }
-
-});
+router.post('/register-user',userRegister);
 
 
 module.exports = router;
-  
